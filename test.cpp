b7b08e21ed7e06979893a7fdba6fd928b35d4347
goosari0123 <goosari0123@gmail.com>
2023-03-30 13:43:00 +0900

    gosari0123 2차커밋
    Changes to be committed:
    	modified:   test.cpp

--- test.cpp
+++ test.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(){
     cout << "테스트 코드" << endl;
     cout << "gosari0123 테스트 코드 추가(1)" << endl;
-
+    cout << "gosari0123 테스트 코드 추가(2)" << endl;
+    
     return 0;
 }
